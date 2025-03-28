f7033549fb48fb2ac4b8e721374d5c4aa0a859e8
Mohd Farman <kfarman9502@gmail.com>
2025-03-28 06:57:57 +0530

    Add more msgs

--- first.java
+++ first.java
@@ -4,6 +4,9 @@ public class first{
         System.out.println("Hello world:");
         System.out.println("Hello world:");
         System.out.println("Hello world:");
+        System.out.println("Hello world:");
+        System.out.println("Hello world:");
+        System.out.println("Hello world:");
     }
     public static void main(String args[]){
         printhello();
