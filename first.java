5905ff412d8e0d18467e16316ff780b750093508
Mohd Farman <kfarman9502@gmail.com>
2025-03-28 06:53:23 +0530

    upload fike

---
+++ first.java
@@ -0,0 +1,12 @@
+public class first{
+    public static void printhello(){
+        System.out.println("Hello world:");
+        System.out.println("Hello world:");
+        System.out.println("Hello world:");
+        System.out.println("Hello world:");
+    }
+    public static void main(String args[]){
+        printhello();
+
+    }
+}
